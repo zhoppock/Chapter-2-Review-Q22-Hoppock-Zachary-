0a4674d76b9e0679a49764a64eff3918205f478d
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-26 01:23:48 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//Zachary Hoppock
+//mathErrors.cpp
+//9/13/2020
+//Deteremine the results fo each of these calculations
+//Version  #1
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  
+  cout << 2.0 - 1.1 << endl;
+
+  cout << 1.0E6 * 1.0E6 << endl;
+
+  cout << 65536 * 65536 << endl;
+
+  cout << 1000000 * 1000000;
+
+  return 0;
+}
